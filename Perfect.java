c59d03b7de1b2cac66d91bca083f399ed24938ec
jamesahn0720 <76964759+jamesahn0720@users.noreply.github.com>
2021-03-15 22:30:29 -0700

    Update Perfect.java

--- Perfect.java
+++ Perfect.java
@@ -4,8 +4,30 @@
 
 public class Perfect
 {
-	public static boolean isPerfect( int number )
-	{
-		return false;
-	}
+ private int number;
+
+    public Perfect(int number) {
+        this.number = number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public boolean isPerfect() {
+        int sum = 0;
+        for(int i = 1; i < number; ++i) {
+            if(number % i == 0) {
+                sum += i;
+            }
+        }
+        return sum == number;
+    }
+	 public String toString() {
+        if(isPerfect()) {
+            return number + " is perfect.";
+        } else {
+            return number + " is not perfect.";
+        }
+    }
 }
