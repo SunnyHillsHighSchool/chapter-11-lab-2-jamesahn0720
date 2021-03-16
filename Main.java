7420e197b576b959d7cb5aaad4d83998b74ca01c
jamesahn0720 <76964759+jamesahn0720@users.noreply.github.com>
2021-03-15 22:30:47 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 class Main {
   public static void main(String[] args) {
-    //add test cases
+   System.out.println(new Perfect(496));
+        System.out.println(new Perfect(45));
+        System.out.println(new Perfect(6));
+        System.out.println(new Perfect(14));
+        System.out.println(new Perfect(8128));
+        System.out.println(new Perfect(1245));
+        System.out.println(new Perfect(33));
+        System.out.println(new Perfect(28));
+        System.out.println(new Perfect(27));
+        System.out.println(new Perfect(33550336));
   }
 }
